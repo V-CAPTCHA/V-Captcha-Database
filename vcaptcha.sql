1a95df17cc358db9b88bd3dd4a0fa74e7cc0747f
THANATATH <thanatuth.euro@gmail.com>
2022-04-05 01:03:46 +0700

    update SQL file

--- vcaptcha.sql
+++ vcaptcha.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.1.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: mysql
--- Generation Time: Feb 17, 2022 at 11:42 AM
+-- Generation Time: Apr 04, 2022 at 06:03 PM
 -- Server version: 8.0.26
--- PHP Version: 7.4.20
+-- PHP Version: 8.0.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,11 +29,11 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `admin` (
   `admin_id` int NOT NULL COMMENT 'รหัสประจำตัวผู้ดูแลระบบ',
-  `email` varchar(50) COLLATE utf8_unicode_ci NOT NULL COMMENT 'อีเมลล์ผู้ดูแลระบบ',
-  `password` varchar(100) COLLATE utf8_unicode_ci NOT NULL COMMENT 'รหัสผ่านผู้ดูแลระบบ',
-  `first_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL COMMENT 'ชื่อผู้ดูแลระบบ',
-  `last_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL COMMENT 'นามสกุลผู้ดูแลระบบ',
-  `role` varchar(50) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'admin' COMMENT 'ตำแหน่งผู้ดูแลระบบ'
+  `email` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL COMMENT 'อีเมลล์ผู้ดูแลระบบ',
+  `password` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL COMMENT 'รหัสผ่านผู้ดูแลระบบ',
+  `first_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL COMMENT 'ชื่อผู้ดูแลระบบ',
+  `last_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL COMMENT 'นามสกุลผู้ดูแลระบบ',
+  `role` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT 'admin' COMMENT 'ตำแหน่งผู้ดูแลระบบ'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci COMMENT='ผู้ดูแลระบบ';
 
 -- --------------------------------------------------------
@@ -81,6 +81,31 @@ CREATE TABLE `dataset` (
   `dataset_question` text NOT NULL COMMENT 'คำถามสำหรับยืนยันตัวตน',
   `dataset_reply` text NOT NULL COMMENT 'คำตอบสำหรับยืนยันตัวตน'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='ข้อมูลสำหรับใช้ยืนยันตัวตน ';
+
+--
+-- Dumping data for table `dataset`
+--
+
+INSERT INTO `dataset` (`dataset_id`, `dataset_img`, `dataset_question`, `dataset_reply`) VALUES
+(38, 'e9ac6643468e40e06f45b5c414630e77.jpg', 'ตัวอะไรเกาะอยู่บนกิ่งไม้', 'นก,นด,เบิร์ด,เบิด,bird'),
+(39, '82753d99ef54a0ee2aac1df5e75929c8.jpg', 'ผู้ชายกำลังนั่งอยู่บนอะไร', 'เก้าอี้,เก้าอี้สีขาว,เกาอี,เก้าอี,9 อี้'),
+(40, 'bf56c08c6f864af5fbff6bd778d5ee80.jpg', 'หมากำลังลากอะไร', 'เลื่อน,เลือน,ที่นั่ง,คน,เจ้าของ'),
+(41, '96c0573ef5bf9bfb57e7cd666fd058ea.jpg', 'เสาในรูปเขียนว่าอะไร', 'แปซิฟิก,แปซิฟิด,แปซิบฟิก,แพซิฟิก,แปสิฟิก'),
+(42, 'ce908a00bdd018a20550c910d7758b2d.jpg', 'อะไรกำลังเกาะที่คอผู้หญิง', 'ผีเสื้อ,บัตเตอร์ฟาย,butterfly,Butterfly,แมลง'),
+(43, '5de2260d2536b1a20f0a3466d39ea55b.jpg', 'นกกำลังเกาะอยู่บนอะไร', 'กิ่งไม้,ต้นไม้,แท่งไม้,ไม้,ก้านไม้'),
+(44, '73a8698849af7825fd35c24aeb2c2fe8.jpg', 'มีคนกี่คนยืนอยู่', '3,Three,three,3 คน,สาม'),
+(45, '5d22d85bfe666acbb78bcbaed0ba485d.jpg', 'ผู้ชายใส่อะไรที่ข้อมือ', 'นาฬิกา,นาฬิกาข้อมือ,Watch,นาริกา,นาลิกา'),
+(46, '7b327265d50c39f630b623033922eb10.jpg', 'มีคนใส่แว่นในรูปกี่คน', '2,สองคน,2 คน,สอง'),
+(47, 'e0212d263a97f952388e8f51600bd5ce.jpg', 'ผู้หญิงถ่ายรูปหน้าอะไร', 'กระจก,มิลเลอร์,Mirror,mirror,กระจกเงา'),
+(48, '1e82a748b3164153a80cb0f3fd09ab70.jpg', 'บ้านในรูปอาศัยได้หรือไม่', 'ได้,อยู่ได้,อาศัยได้,อยู่อาศัยได้'),
+(49, '8fc51b3d15812a0dea4bce45fc1d3bce.jpg', 'คนในรูปกำลังเล่นมือถือหรือไม่', 'ไม่,ไม่เล่น,ไม่เล่นอยู่,เล่นคอมอยู่,ไม่เล่นมือถือ'),
+(50, 'd8f6d2704c6b695c433a299c89c71446.jpg', 'ในรูปเป็นคำว่าอะไร', 'เลิฟ,เลิป,แอลโอวีอี,Love'),
+(51, 'c541b2e636fb6c3389bbd4d5587e14c2.jpg', 'ในรูปมีบรรไดหรือไม่', 'มี,มีบรรได,บรรได'),
+(52, '7b629dd84921d961bc0127c11cb0d3f1.jpg', 'รถกำลังแล่นอยู่หรือไม่', 'ไม่,ไม่แล่น,ไม่แล่นอยู่,จอดอยู่,หยุด'),
+(53, '7128e04181d42994876c9185490e220f.jpg', 'หมาในรูปใส่เสื้อสีอะไร', 'เหลือง,yellow,เหลิง,เลิง,สีเหลือง'),
+(54, '425d5ed7eef565039076498cc6d05f37.jpg', 'ผู้ชายซ้ายมือใส่เสื้อสีอะไร', 'บลู,น้ำเงิน,สีน้ำเงิน,ฟ้า,สีฟ้า'),
+(55, 'a72761a3f46c0d19ea4561a29aab96b5.jpg', 'บ้านหลังนี้อยู่อาศัยได้หรือไม่', 'ไม่ได้,อยู่ไม่ได้,อยู่อาศัยไม่ได้,ไม่'),
+(56, 'aca4eb81d1d80160b4261fbfb634ec99.jpg', 'ในรูปมีสิ่งมีชีวิตหรือไม่', 'ไม่มี,ไม่มีสิ่งมีชีวิต,ไม่,ไม่ใช่');
 
 -- --------------------------------------------------------
 
@@ -181,7 +206,7 @@ ALTER TABLE `captcha_key`
 -- AUTO_INCREMENT for table `dataset`
 --
 ALTER TABLE `dataset`
-  MODIFY `dataset_id` int NOT NULL AUTO_INCREMENT COMMENT 'รหัสข้อมูล dataset ที่ใช้ในการยืนยันตัวตน';
+  MODIFY `dataset_id` int NOT NULL AUTO_INCREMENT COMMENT 'รหัสข้อมูล dataset ที่ใช้ในการยืนยันตัวตน', AUTO_INCREMENT=57;
 
 --
 -- AUTO_INCREMENT for table `dataset_creator`
